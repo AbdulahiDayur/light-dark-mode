a58d63b6e8d3f3740645ce7aaa479b1f2253b89e
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-03-09 21:31:53 -0800

    initial project structure

---
+++ script.js
@@ -0,0 +1,10 @@
+// async function getAPI() {
+//   const apiURL = ''
+//   try {
+//     const res = await axios.get(apiURL);
+//     const data = await res.data;
+
+//   } catch (error) {
+//     console.log('Whoops', error);
+//   }
+// }
